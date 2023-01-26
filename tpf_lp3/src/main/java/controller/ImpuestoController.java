90205ec0993d7088482e196fbd20e7d0c24baceb
Christian-Arce <christian55501@gmail.com>
2023-01-26 11:25:01 -0300

    Usuario controller

--- tpf_lp3/src/main/java/controller/ImpuestoController.java
+++ tpf_lp3/src/main/java/controller/ImpuestoController.java
@@ -1,7 +1,7 @@
 package controller;
 
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,7 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +45,7 @@ public class ImpuestoController {
 	}
 	
 	@PutMapping(
-			value = "/editatImpuesto",
+			value = "/editarImpuesto",
 			consumes = {MediaType.APPLICATION_JSON_VALUE},
 			produces = {MediaType.APPLICATION_JSON_VALUE})
 	public Boolean editarImpuesto(@RequestBody Impuesto i1) {
